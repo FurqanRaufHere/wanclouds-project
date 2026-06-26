ec6ae273dd06ca45a39a4efffc2e4e7ff43164b7
Muhammad Furqan Rauf <furqanrauf6@gmail.com>
2026-06-26 19:56:06 +0500

    Update

--- alembic/env.py
+++ alembic/env.py
@@ -17,7 +17,7 @@ from app.core.config import DATABASE_URL
 config = context.config
 
 if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+    fileConfig(config.config_file_name, disable_existing_loggers=False)
 
 config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
